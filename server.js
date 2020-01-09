ee31c60efceedc871c2dfd6ff06bd178252e93a2
rishabhgarg25699 <rishabhgarg25699@gmail.com>
2020-01-09 09:29:13 +0530

    added

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const server = express();
+server.get('/', function (req, res) {
+    console.log("<h1><center> First server </h1></center>");
+
+    res.send("<h1><center> First server </h1></center>")
+});
+
+server.listen(5000);
